cbfc09b0eb2f4c726711b23e10655f277e1180d3
Laurent Jouron <jouronlaurent@hotmail.com>
2023-05-23 13:39:20 +0200

    add print lower user

--- authentication/views.py
+++ authentication/views.py
@@ -15,6 +15,7 @@ class SignupView(View):
 
     template = 'authentication/signup.html'
     form = forms.SignupForm
+    model = models.User()
 
     def get(self, request):
         form = self.form()
@@ -25,9 +26,13 @@ class SignupView(View):
         form = self.form(request.POST)
         if form.is_valid():
             print(form.data["username"].lower())
+            user = form.save(commit=False)
+            print(user.username.lower())
+            user = models.User()
+            print(user)
 
-            user = form.save()
-            login(request, user)
+            # user = form.save()
+            # login(request, user)
             return redirect(settings.LOGIN_REDIRECT_URL)
         context = {'form': form}
         return render(request, self.template, context=context)
@@ -52,25 +57,23 @@ class LoginView(View):
         )
 
     def post(self, request):
-        if request.method == 'POST':
-            form = self.form(request.POST)
-            message = ''
-            if form.is_valid():
-                user = authenticate(
-                    username=form.cleaned_data['username'],
-                    password=form.cleaned_data['password'],
-                )
-                if user is not None:
-                    login(request, user)
-                    return redirect(settings.LOGIN_REDIRECT_URL)
-                else:
-                    message = 'Identifiants incorrects'
-            context = {'form': form, 'message': message}
-            return render(
-                request,
-                self.template,
-                context=context,
+        form = self.form(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
             )
+            if user is not None:
+                login(request, user)
+                return redirect(settings.LOGIN_REDIRECT_URL)
+
+        message = 'Identifiants incorrects'
+        context = {'form': form, 'message': message}
+        return render(
+            request,
+            self.template,
+            context=context,
+        )
 
 
 def logout_user(request):
